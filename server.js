54d17ad03773634fbd2dbe17af5d64375eb53316
Basharirani23 <super_boshbosh@hotmail.com>
2023-05-04 22:46:17 +0300

    Lab 11

---
+++ server.js
@@ -0,0 +1,79 @@
+"use strict";
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+//app.get("/", (req, res) => {
+//  console.log(`testing the first URL`);
+//  res.send(`Testing the First`);
+//});
+//app.listen(3003, () => console.log(`Up and Running on port 3003`));
+
+//app.get("/", handleHome);
+//app.get("/favorite", handleFav);
+//******** */
+
+
+//Create a route with a method of get and a path of /. The callback should use the provided JSON data.
+const jsonData = require("./Movie Data/data.json");
+app.get("/", (req, res) => {
+  res.json(jsonData);
+});
+app.listen(3003, () => {
+  console.log("Server listening on port 3003");
+});
+
+//Create a constructor function to ensure your data follow the same format.
+
+function Movie(title, posterPath, overview) {
+  this.title = title;
+  this.poster_path = posterPath;
+  this.overview = overview;
+  this.original_language = original_language;
+  this.getInfo = function () {
+    return {
+      title: this.title,
+      poster_path: this.poster_path,
+      overview: this.overview,
+      original_language: this.original_language,
+    };
+  };
+}
+
+
+//Favorite Page Endpoint: “/favorite”
+
+app.get("/favorite", (req, res) => {
+  res.send("Welcome to Favorite Page");
+});
+
+// handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).json({
+    statusCode: 404,
+    message: "Page not found!",
+  });
+});
+// handle 500 errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    statusCode: 500,
+    message: "Internal server error!",
+  });
+});
+
+function handleFav(req, res) {
+  console.log("testing the favorite url");
+  res.send("welcome to favorites");
+}
+
+//error 500 test here in home
+function handleHome(req, res) {
+  const jsonData = require("./Movie Data/data.json");
+  const index = req.query.index;
+  const movie = jsonData[index];
+  const newMovie = new Movie(movie.title, movie.poster_path, movie.overview);
+  res.json({ newMovie: newMovie });
+}
