4e70b46c8d3fb59c3b8fa0948f916bd632959436
Basharirani23 <super_boshbosh@hotmail.com>
2023-05-08 14:12:18 +0300

    Edits done

--- server.js
+++ server.js
@@ -14,36 +14,15 @@ app.use(cors());
 //app.get("/favorite", handleFav);
 //******** */
 
-
 //Create a route with a method of get and a path of /. The callback should use the provided JSON data.
 const jsonData = require("./Movie Data/data.json");
+
 app.get("/", (req, res) => {
-  res.json(jsonData);
+  let test = new Movie(jsonData);
+  res.json(test);
 });
-app.listen(3003, () => {
-  console.log("Server listening on port 3003");
-});
-
-//Create a constructor function to ensure your data follow the same format.
-
-function Movie(title, posterPath, overview) {
-  this.title = title;
-  this.poster_path = posterPath;
-  this.overview = overview;
-  this.original_language = original_language;
-  this.getInfo = function () {
-    return {
-      title: this.title,
-      poster_path: this.poster_path,
-      overview: this.overview,
-      original_language: this.original_language,
-    };
-  };
-}
-
 
 //Favorite Page Endpoint: “/favorite”
-
 app.get("/favorite", (req, res) => {
   res.send("Welcome to Favorite Page");
 });
@@ -64,16 +43,33 @@ app.use((err, req, res, next) => {
   });
 });
 
-function handleFav(req, res) {
-  console.log("testing the favorite url");
-  res.send("welcome to favorites");
+//Create a constructor function to ensure your data follow the same format.
+function Movie(ex) {
+  this.title = ex.title;
+  this.poster_path = ex.poster_path;
+  this.overview = ex.overview;
+  return this;
 }
 
-//error 500 test here in home
-function handleHome(req, res) {
-  const jsonData = require("./Movie Data/data.json");
-  const index = req.query.index;
-  const movie = jsonData[index];
-  const newMovie = new Movie(movie.title, movie.poster_path, movie.overview);
-  res.json({ newMovie: newMovie });
-}
+//Listen
+app.listen(3003, () => {
+  console.log("Server listening on port 3003");
+});
+
+
+
+
+
+// function handleFav(req, res) {
+//   console.log("testing the favorite url");
+//   res.send("welcome to favorites");
+// }
+
+// //error 500 test here in home
+// function handleHome(req, res) {
+//   const jsonData = require("./Movie Data/data.json");
+//   const index = req.query.index;
+//   const movie = jsonData[index];
+//   const newMovie = new Movie(movie.title, movie.poster_path, movie.overview);
+//   res.json({ newMovie: newMovie });
+// }
