2cdc54ae8f8fdac67067c4965aeba5386a178e15
Basharirani23 <super_boshbosh@hotmail.com>
2023-05-13 18:04:13 +0300

    edits

--- server.js
+++ server.js
@@ -3,46 +3,37 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-
-//app.get("/", (req, res) => {
-//  console.log(`testing the first URL`);
-//  res.send(`Testing the First`);
-//});
-//app.listen(3003, () => console.log(`Up and Running on port 3003`));
-
-//app.get("/", handleHome);
-//app.get("/favorite", handleFav);
-//******** */
+app.get("/", handleHome);
+app.get("/favorite", handleFavorite);
 
 //Create a route with a method of get and a path of /. The callback should use the provided JSON data.
 const jsonData = require("./Movie Data/data.json");
 
-app.get("/", (req, res) => {
+//Home Page Endpoint: “/”
+function handleHome(req, res) {
   let test = new Movie(jsonData);
   res.json(test);
-});
- 
+}
 //Favorite Page Endpoint: “/favorite”
-app.get("/favorite", (req, res) => {
+function handleFavorite(req, res) {
   res.send("Welcome to Favorite Page");
-});
+}
 
 // handle 404 errors
-app.use((req, res, next) => {
+function handle404(req, res, next) {
   res.status(404).json({
     statusCode: 404,
     message: "Page not found!",
   });
-});
+}
 
 // handle 500 errors
-app.use((err, req, res, next) => {
-  console.error(err.stack);
+function handle500(err, req, res, next) {
   res.status(500).json({
     statusCode: 500,
     message: "Internal server error!",
   });
-});
+}
 
 //Create a constructor function to ensure your data follow the same format.
 function Movie(ex) {
@@ -56,21 +47,3 @@ function Movie(ex) {
 app.listen(3003, () => {
   console.log("Server listening on port 3003");
 });
-
-
-
-
-
-// function handleFav(req, res) {
-//   console.log("testing the favorite url");
-//   res.send("welcome to favorites");
-// }
-
-// //error 500 test here in home
-// function handleHome(req, res) {
-//   const jsonData = require("./Movie Data/data.json");
-//   const index = req.query.index;
-//   const movie = jsonData[index];
-//   const newMovie = new Movie(movie.title, movie.poster_path, movie.overview);
-//   res.json({ newMovie: newMovie });
-// }
